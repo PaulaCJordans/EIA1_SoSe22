2c5f217ad7efe97387beba801c3bf0770a32cae1
PaulaCJordans <paula.jordans@gmx.de>
2022-05-18 13:38:06 +0200

    Hochladen Aufgabe7 und Steckbrief

---
+++ Aufgaben/Aufgabe8/drumpad.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", addClickListenersDrumPad);
+function playSample(soundQuelle) {
+    var sound = new Audio(soundQuelle);
+    sound.play();
+}
+;
+function addClickListenersDrumPad() {
+    document.querySelector("#button1").addEventListener("click", function () { playSample("./assets/DrumPad/A.mps"); });
+    document.querySelector("#button2").addEventListener("click", function () { playSample("./assets/DrumPad/C.mp3"); });
+    document.querySelector("#button3").addEventListener("click", function () { playSample("./assets/DrumPad/F.mp3"); });
+    document.querySelector("#button4").addEventListener("click", function () { playSample("./assets/DrumPad/G.mps"); });
+    document.querySelector("#button5").addEventListener("click", function () { playSample("./assets/DrumPad/hihat.mp3"); });
+    document.querySelector("#button6").addEventListener("click", function () { playSample("./assets/DrumPad/laugh-1.mp3"); });
+    document.querySelector("#button7").addEventListener("click", function () { playSample("./assets/DrumPad/laugh-2.mp3"); });
+    document.querySelector("#button8").addEventListener("click", function () { playSample("./assets/DrumPad/snare.mp3"); });
+    document.querySelector("#button9").addEventListener("click", function () { playSample("./assets/DrumPad/kick.mp§"); });
+    document.querySelector(".play").addEventListener("click'", function () { playBeat(); });
+}
+;
+var beat = ["assets/DrumPad/kick.mp3", "assets/DrumPad/snare.mp3", "assets/DrumPad/hihat.mp3"];
+var zaehler = 0;
+document.querySelector(".play").addEventListener("click", playBeat);
+setInterval(function playBeat() {
+    playSample(beat[zaehler]);
+    zaehler = zaehler + 1;
+    if (zaehler === 3) {
+        zaehler = 0;
+    }
+    //Anweisungen
+}, 500);
+function playBeat() {
+    playSample(beat[zaehler]);
+    zaehler = zaehler + 1;
+    if (zaehler === 3) {
+        zaehler = 0;
+    }
+    setInterval(function () {
+        // Anweisungen 
+    }, 500);
+}
+//# sourceMappingURL=drumpad.js.map
